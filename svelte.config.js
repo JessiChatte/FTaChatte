113d4961e55024e4e636a9367869593dad8aa2a0
JessiChatte <jessica.shemyr@gmail.com>
2024-09-28 16:58:02 +0200

    #1 Add shadcn to handle ui components

--- svelte.config.js
+++ svelte.config.js
@@ -9,6 +9,11 @@ const config = {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
+    alias: {
+      $components: "./src/components",
+      $lib: "./src/lib",
+      $stores: "./src/stores",
+    },
   },
 };
 
