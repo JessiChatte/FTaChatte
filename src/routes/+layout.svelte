29aa4dab7442136f9ee62c23e53e9e916e5d812a
JessiChatte <jessica.shemyr@gmail.com>
2024-10-03 19:02:57 +0200

    #5 Add UI elements and migrate to tauri 2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,8 @@
-<script>import "../app.css";</script><slot></slot>
+<script lang="ts">
+    import "../app.css";
+    import { ModeWatcher, toggleMode } from "mode-watcher";
+    import {Button} from "$components/ui/button";
+</script>
+<ModeWatcher />
+<Button on:click={toggleMode}>Theme</Button>
+<slot></slot>
